93c26765b3395288fc91629f1a055a68e4c56f64
anyway <zorix228337@gmail.com>
2026-06-17 17:44:13 +0300

    add info about C

---
+++ src/script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('.info-buttons');
+const blocks = document.querySelectorAll('.info-block');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        blocks.forEach(block => block.classList.remove('active'));
+
+        buttons.forEach(btn => btn.classList.remove('active'));
+
+        const targetId = button.getAttribute('data-target');
+
+        const target = document.getElementById(targetId);
+        if(target){
+            target.classList.add('active');
+            button.classList.add('active');
+        }
+    })
+})
